38f7534f06533214c195164920b2cfb81e2c8d6c
alairfonseca <alair@rarolabs.com.br>
2022-02-09 14:01:06 -0300

    binary search

--- ch4/4_1_3.py
+++ ch4/4_1_3.py
@@ -1,5 +1,22 @@
+# Binary search
+def binary_search(data, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
 
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search(data, target, low, mid - 1)
+        else:
+            return binary_search(data, target, mid + 1, high)
 
 
 if __name__ == "__main__":
-    print()
+    list1 = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+    ]
+    list2 = list(range(1, 2000))
+    print(binary_search(list1, 13, 0, len(list1) - 1))
+    print(binary_search(list1, 21, 0, len(list1) - 1))
