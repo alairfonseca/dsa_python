98f01946c6458f4853b0777c326a320f5fae84bf
alairfonseca <alair@rarolabs.com.br>
2022-02-17 18:31:55 -0300

    .

--- recursion/power.py
+++ recursion/power.py
@@ -1,4 +1,6 @@
 def power(n, p):
+    if p == 0:
+        return 1
     if p == 1:
         return n
     return n * power(n, p - 1)
@@ -7,3 +9,4 @@ def power(n, p):
 if __name__ == "__main__":
     print(power(2, 3))
     print(power(4, 6))
+    print(power(2, 0))
