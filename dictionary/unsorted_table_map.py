b59f7ed0837c273a2ae9f41f1a8c6510b349b7be
alairfonseca <alair@rarolabs.com.br>
2022-02-20 17:19:41 -0300

    unsorted table

---
+++ dictionary/unsorted_table_map.py
@@ -0,0 +1,35 @@
+from map_base import MapBase
+
+class UnsortedTableMap(MapBase):
+    def __init__(self):
+        self._table = []
+
+    def __getitem__(self, k):
+        for item in self._table:
+            if k == item._key:
+                return item._value
+
+        raise KeyError('Key errorK ' + repr(k))
+    
+    def __setitem__(self, k, v):
+        for item in self._table:
+            if k == item._key:
+                item._value = v
+                return
+
+        self._table.append(self._item(k,v))
+
+    def __delitem__(self, k):
+        for i in range(len(self._table)):
+            if k == self._table[i]._key:
+                self._table.pop(i)
+                return
+
+        raise KeyError('Key errorK ' + repr(k))
+
+    def __len__(self):
+        return len(self._table)
+
+    def __iter__(self):
+        for item in self._table:
+            yield item._key
