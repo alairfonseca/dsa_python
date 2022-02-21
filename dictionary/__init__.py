4b2ad2b7769024e5c2f0123039d3a5dcebcf45ca
alairfonseca <alair@rarolabs.com.br>
2022-02-21 14:49:57 -0300

    hash map impl

---
+++ dictionary/__init__.py
@@ -0,0 +1,38 @@
+from unsorted_table_map import UnsortedTableMap
+from chain_hash_map import ChainHashMap
+
+# cyclic-shift hash code
+def hash_code(s):
+    mask = (1 << 32) - 1
+    h = 0
+
+    for char in s:
+        h = (h << 5 & mask) | (h >> 27)
+        h += ord(char)
+
+    return h
+
+
+if __name__ == "__main__":
+    print(hash_code("test"))
+    print(hash_code("test1"))
+    print(hash_code("test11"))
+    print("============================")
+    
+    chmap = ChainHashMap()
+
+    chmap["name"] = "chain hash map"
+    chmap["test"] = "test.."
+    chmap["test1"] = "test..."
+    chmap["test11"] = "test...."
+
+    for i in chmap:
+        print(chmap[i])
+
+    print("============================")
+    del chmap["test1"]
+
+    for i in chmap:
+        print(chmap[i])
+
+
