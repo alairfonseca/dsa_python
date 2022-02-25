23047616a2dd756e07375c5fc554232f293922d6
alairfonseca <alair@rarolabs.com.br>
2022-02-25 16:02:34 -0300

    traversing bst

---
+++ tree/binary_search_tree.py
@@ -0,0 +1,82 @@
+from cqueue import CQueue
+
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def insert(root, value):
+    if root.data == None:
+        root.data = value
+    elif value <= root.data:
+        if root.left == None:
+            root.left = BSTNode(value)
+        else:
+            insert(root.left, value)
+    else:
+        if root.right == None:
+            root.right = BSTNode(value)
+        else:
+            insert(root.right, value)
+
+def preorder_traversal(root):
+    if not root:
+        return
+    
+    print(root.data)
+    preorder_traversal(root.left)
+    preorder_traversal(root.right)
+
+def inorder_traversal(root):
+    if not root:
+        return
+    
+    inorder_traversal(root.left)
+    print(root.data)
+    inorder_traversal(root.right)
+
+def postorder_traversal(root):
+    if not root:
+        return
+    
+    postorder_traversal(root.left)
+    postorder_traversal(root.right)
+    print(root.data)
+
+def levelorder_traversal(root):
+    if not root:
+        return
+    q = CQueue()
+    q.enqueue(root)
+
+    while not q.is_empty():
+        root = q.dequeue()
+        print(root._value._value.data)
+
+        if root._value._value.left is not None:
+            q.enqueue(root._value._value.left)
+        if root._value._value.right is not None:
+            q.enqueue(root._value._value.right)
+
+if __name__ == "__main__":
+    bst = BSTNode(None)
+
+    insert(bst, 70)
+    insert(bst, 50)
+    insert(bst, 90)
+    insert(bst, 40)
+    insert(bst, 60)
+    insert(bst, 65)
+    insert(bst, 50)
+    insert(bst, 45)
+    insert(bst, 43)
+
+    preorder_traversal(bst)
+    print("--------------------")
+    inorder_traversal(bst)
+    print("--------------------")
+    postorder_traversal(bst)
+    print("--------------------")
+    levelorder_traversal(bst)
+    print("--------------------")
