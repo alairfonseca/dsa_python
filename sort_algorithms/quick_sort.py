bafe608fea35b565262727a334882898ab102252
alairfonseca <alair@rarolabs.com.br>
2022-03-01 12:50:44 -0300

    quick sort

--- sort_algorithms/quick_sort.py
+++ sort_algorithms/quick_sort.py
@@ -1,52 +1,26 @@
-def merge(sequence, i, m, r):
-    n1 = m - l + 1
-    n2 = r - m
+def partition(sequence, low, high):
+    i = low - 1
+    pivot = sequence[high]
 
-    L = [0] * n1
-    R = [0] * n2
-
-    for i in range(n1):
-        L[i] = sequence[l+i]
-    
-    for i in range(n2):
-        R[i] = sequence[m + i + 1]
-
-    i = 0
-    j = 0
-    k = l
-
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            sequence[k] = L[i]
+    for j in range(low, high):
+        if sequence[j] < pivot:
             i += 1
-        else:
-            sequence[k] = R[j]
-            j += 1
-
-        k += 1
-
-    while i < n1:
-        sequence[k] = L[i]
-        i += 1
-        k += 1
-
-    while j < n2:
-        sequence[k] = R[j]
-        j += 1
-        k += 1
-
-def merge_sort(sequence, l, r):
-    if l < r:
-        m = (l + (r - 1)) // 2
-        merge_sort(sequence, l, m)
-        merge_sort(sequence, m + 1, r)
-        merge(sequence, l, m, r)
+            sequence[i], sequence[j] = sequence[j], sequence[i]
+    sequence[i + 1], sequence[high] = sequence[high], sequence[i + 1]
     
+    return i + 1    
+
+def quick_sort(sequence, low, high):
+    if low < high:
+        pindex = partition(sequence, low, high)
+        quick_sort(sequence, low, pindex - 1)
+        quick_sort(sequence, pindex + 1, high)
+
     print(sequence)
 
 if __name__ == "__main__":
     l = [2, 1, 7, 8, 6, 5, 9, 3, 4]
     l2 = [64, 34, 25, 12, 22, 11, 90]
 
-    merge_sort(l, 0, len(l) - 1)
-    merge_sort(l2, 0, len(l2) - 1)
+    quick_sort(l, 0, len(l) - 1)
+    quick_sort(l2, 0, len(l2) - 2)
