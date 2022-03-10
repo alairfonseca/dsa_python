d8f3398abf406fcb09b56043814a6c0fb21849ad
alairfonseca <alair@rarolabs.com.br>
2022-03-10 14:24:49 -0300

    3

--- exercises/two_pointers.py
+++ exercises/two_pointers.py
@@ -28,17 +28,47 @@ def remove_duplicates(arr):
 
     return len(arr) - duplicates
 
+# Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+def make_squares(arr):
+    result = [0] * len(arr)
+    a = 0
+    b = len(arr) - 1
+    current_insert_index = b
+
+    while a < b:
+        sqra = abs(arr[a] ** 2)
+        sqrb = abs(arr[b] ** 2)
+
+        if sqra > sqrb:
+            result[current_insert_index] = sqra
+            result[current_insert_index - 1] = sqrb
+        else:
+            result[current_insert_index] = sqrb
+            result[current_insert_index - 1] = sqra
+
+        current_insert_index -= 2
+
+        b -= 1
+        a += 1
+
+    return result
+
 if __name__ == "__main__":
     arrays = [[1, 2, 3, 4, 6], [2, 5, 9, 11]]
     ts = [6, 11]
     for (i, a) in enumerate(arrays):
         pair_with_targetsum(a, ts[i])
+
     print("===========================")
     arrays = [[2, 3, 3, 3, 6, 9, 9], [2, 2, 2, 11], [1, 2, 3, 4, 5, 6]]
     for a in arrays:
         print(remove_duplicates(a))
 
     print("===========================")
+    arrays = [[-2, -1, 0, 2, 3], [-3, -1, 0, 1, 2]]
+    for a in arrays:
+        print(make_squares(a))
+    
     print("===========================")
     print("===========================")
     print("===========================")
