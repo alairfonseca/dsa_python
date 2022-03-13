d8f4bf413a0f475dca0cf2fec2f70a0753abc457
alairfonseca <alair@rarolabs.com.br>
2022-03-13 16:32:41 -0300

    3

--- exercises/inplace_linked_list_rev.py
+++ exercises/inplace_linked_list_rev.py
@@ -24,6 +24,41 @@ def reverse(head):
 
     return previous
 
+# Given the head of a LinkedList and two positions ‘p’ and ‘q’, reverse the LinkedList from position ‘p’ to ‘q’.
+def reverse_sub_list(head, p, q):
+    start = None
+    end = None
+
+    cursor = head
+    while not start or not end:
+        if cursor.next.value == p:
+            start = cursor
+
+        if cursor.value == q:
+            end = cursor.next
+        
+        cursor = cursor.next
+
+    current = start.next
+    previous = None
+    next = None
+    while current.value <= q:
+        next = current.next
+
+        if not previous:
+            current.next = end
+            previous = current
+        else:
+            current.next = previous
+            previous = current
+        
+        current = next
+
+    start.next = previous
+
+    return head
+
+
 def main():
     head = Node(2)
     head.next = Node(4)
@@ -34,6 +69,18 @@ def main():
     print("Nodes of original LinkedList are: ", end='')
     head.print_list()
     result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse_sub_list(head, 2, 4)
     print("Nodes of reversed LinkedList are: ", end='')
     result.print_list()
 
