96d77710d9918b2f9aab346da5efcb0ab0a1b9ce
alairfonseca <alair@rarolabs.com.br>
2022-03-13 14:08:13 -0300

    3

--- exercises/cyclic_sort.py
+++ exercises/cyclic_sort.py
@@ -30,6 +30,26 @@ def find_missing_number(nums):
 
     return no_missing_sum - actual_sum
 
+# We are given an unsorted array containing numbers taken from the range 1 to ‘n’. The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+def find_missing_numbers(nums):
+    missingNumbers = []
+    i = 0
+
+    while i < len(nums):
+        value = nums[i]
+
+        if value - 1 != i and nums[value - 1] != nums[i]:
+            nums[i], nums[value - 1] = nums[value - 1], nums[i]
+        else:
+            i += 1
+
+    for (i, v) in enumerate(nums):
+        if v - 1 != i:
+            missingNumbers.append(i + 1)
+
+    return missingNumbers
+
+
 if __name__ == "__main__":
     arrays = [[3, 1, 5, 4, 2], [2, 6, 4, 3, 1, 5], [1, 5, 6, 4, 3, 2]]
     for a in arrays:
@@ -41,5 +61,9 @@ if __name__ == "__main__":
         print(find_missing_number(a))
 
     print("==========================")
+    arrays = [[2, 3, 1, 8, 2, 3, 5, 1], [2, 4, 1, 2], [2, 3, 2, 1]]
+    for a in arrays:
+        print(find_missing_numbers(a))
+
     print("==========================")
     print("==========================")
