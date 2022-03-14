79e228845aca983026248dfa73c99ded792e9b7f
alairfonseca <alair@rarolabs.com.br>
2022-03-13 22:49:08 -0300

    4

--- exercises/bfs.py
+++ exercises/bfs.py
@@ -86,6 +86,30 @@ def zigzag(root):
 
     return result
 
+
+# Given a binary tree, populate an array to represent the averages of all of its levels.
+def find_level_averages(root):
+    result = []
+    queue = [root]
+
+    while queue:
+        level_size = len(queue)
+        level_sum = 0.0
+        
+        for _ in range(level_size):
+            node = queue.pop(0)
+            level_sum += node.val
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        result.append(level_sum / level_size)
+
+    return result
+
+
 def main():
     root = TreeNode(12)
     root.left = TreeNode(7)
@@ -112,5 +136,14 @@ def main():
     root.right.left.left = TreeNode(20)
     root.right.left.right = TreeNode(17)
     print("Zigzag traversal: " + str(zigzag(root)))
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level averages are: " + str(find_level_averages(root)))
 
 main()
