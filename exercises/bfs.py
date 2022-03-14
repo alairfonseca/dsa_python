4dc384ae38c6264230721d357463b71794055b80
alairfonseca <alair@rarolabs.com.br>
2022-03-13 22:28:26 -0300

    3

--- exercises/bfs.py
+++ exercises/bfs.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -26,6 +28,64 @@ def traverse(root):
 
     return result
 
+"""
+Given a binary tree, populate an array to represent its level-by-level traversal in reverse order,
+i.e., the lowest level comes first. You should populate the values of all nodes in each level from
+left to right in separate sub-arrays.
+"""
+def traverse2(root):
+    result = deque()
+    queue = [root]
+    
+    while queue:
+        level_size = len(queue)
+        current_level = []
+
+        for _ in range(level_size):
+            node = queue.pop(0)
+            current_level.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        result.appendleft(current_level)
+
+    return result
+
+"""
+Given a binary tree, populate an array to represent its zigzag level order traversal.
+You should populate the values of all nodes of the first level from left to right, then
+right to left for the next level and keep alternating in the same manner for the following levels.
+"""
+def zigzag(root):
+    result = []
+    zig = True
+    queue = [root]
+    
+    while queue:
+        level_size = len(queue)
+        current_level = []
+
+        for _ in range(level_size):
+            node = queue.pop(0)
+
+            if zig:
+                current_level.append(node.val)
+            else:
+                current_level.insert(0, node.val)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        result.append(current_level)
+        zig = not zig
+
+    return result
+
 def main():
     root = TreeNode(12)
     root.left = TreeNode(7)
@@ -35,5 +95,22 @@ def main():
     root.right.right = TreeNode(5)
     print("Level order traversal: " + str(traverse(root)))
 
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Reverse level order traversal: " + str(traverse2(root)))
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.right.left.left = TreeNode(20)
+    root.right.left.right = TreeNode(17)
+    print("Zigzag traversal: " + str(zigzag(root)))
 
 main()
