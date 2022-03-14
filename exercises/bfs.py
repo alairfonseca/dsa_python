4c82766436c395b3e54a8487000d0efb2be9ac24
alairfonseca <alair@rarolabs.com.br>
2022-03-13 23:05:00 -0300

    5

--- exercises/bfs.py
+++ exercises/bfs.py
@@ -109,6 +109,26 @@ def find_level_averages(root):
 
     return result
 
+# Find the minimum depth of a binary tree. The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
+def find_minimum_depth(root):
+    queue = [root]
+    level = 0
+
+    while queue:
+        level_size = len(queue)
+        level += 1
+
+        for _ in range(level_size):
+            node = queue.pop(0)
+            
+            if not node.left and not node.right:
+                return level
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
 
 def main():
     root = TreeNode(12)
@@ -145,5 +165,16 @@ def main():
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
     print("Level averages are: " + str(find_level_averages(root)))
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+    root.left.left = TreeNode(9)
+    root.right.left.left = TreeNode(11)
+    print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+
 
 main()
