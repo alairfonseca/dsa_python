a94a598fd7b95f9c9bbcbfe0dad152ecd480d774
alairfonseca <alair@rarolabs.com.br>
2022-03-17 19:33:21 -0300

    7

--- exercises/bfs.py
+++ exercises/bfs.py
@@ -3,8 +3,22 @@ from collections import deque
 class TreeNode:
     def __init__(self, val):
         self.val = val
-        self.left, self.right = None, None
+        self.left, self.right, self.next = None, None, None
 
+    def print_level_order(self):
+        nextLevelRoot = self
+        while nextLevelRoot:
+            current = nextLevelRoot
+            nextLevelRoot = None
+            while current:
+                print(str(current.val) + " ", end='')
+                if not nextLevelRoot:
+                    if current.left:
+                        nextLevelRoot = current.left
+                    elif current.right:
+                        nextLevelRoot = current.right
+                current = current.next
+            print()
 
 # Given a binary tree, populate an array to represent its level-by-level traversal. You should populate the values of all nodes of each level from left to right in separate sub-arrays.
 def traverse(root):
@@ -151,6 +165,26 @@ def find_successor(root, key):
         return None
                 
 
+# Given a binary tree, connect each node with its level order successor. The last node of each level should point to a null node.
+def connect_level_order_siblings(root):
+    queue = [root]
+
+    while queue:
+        previous = None
+        level_size = len(queue)
+
+        for _ in range(level_size):
+            node = queue.pop(0)
+            
+            if previous:
+                previous.next = node
+            previous = node
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
 
 def main():
     root = TreeNode(12)
@@ -210,6 +244,17 @@ def main():
     result = find_successor(root, 9)
     if result:
         print("achou!!", result.val)
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_level_order_siblings(root)
+
+    print("Level order traversal using 'next' pointer: ")
+    root.print_level_order()
 
 
 main()
