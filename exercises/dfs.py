78bc272b938e0b5c13c93f6528bbe5fc128d58c4
alairfonseca <alair@rarolabs.com.br>
2022-03-18 15:32:03 -0300

    2

--- exercises/dfs.py
+++ exercises/dfs.py
@@ -15,6 +15,29 @@ def has_path(root, path_sum):
     return has_path(root.left, path_sum - root.val) or has_path(root.right, path_sum - root.val)
 
 
+# Given a binary tree and a number ‘S’, find all paths from root-to-leaf such that the sum of all the node values of each path equals ‘S’.
+def find_paths(root, path_sum):
+    result = []
+
+    find_paths_rec(root, path_sum, [], result)
+
+    return result
+
+def find_paths_rec(current_node, required_sum, current_path, paths):
+    if not current_node:
+        return
+
+    current_path.append(current_node.val)
+
+    if current_node.val == required_sum and not current_node.left and not current_node.right:
+        paths.append(list(current_path))
+    else:
+        find_paths_rec(current_node.left, required_sum - current_node.val, current_path, paths)
+        
+        find_paths_rec(current_node.right, required_sum - current_node.val, current_path, paths)
+
+    current_path.pop()
+
 def main():
     root = TreeNode(12)
     root.left = TreeNode(7)
@@ -24,6 +47,15 @@ def main():
     root.right.right = TreeNode(5)
     print("Tree has path: " + str(has_path(root, 23)))
     print("Tree has path: " + str(has_path(root, 16)))
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    sum = 23
+    print("Tree paths with sum " + str(sum) + ": " + str(find_paths(root, sum)))
 
 
 main()
